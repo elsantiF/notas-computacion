8fb70852b74990ab24361efc172ca60f3d042846
elsantiF <santiagofarall@gmail.com>
2026-03-08 22:36:41 -0300

    feat: añade rehype-katex y remark-math

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import mdx from "@astrojs/mdx";
 
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), mdx()],
+	integrations: [svelte(), mdx({
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	})],
 });
